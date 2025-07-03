92682cc7933593ace75d988ce2ca91d77b60d28b
alexrus5495 <alexrus5495@gmail.com>
2025-07-03 13:59:31 +0300

    update config files structure

--- src/config/cards.base.bases.ts
+++ src/config/cards.base.bases.ts
@@ -1,20 +1,24 @@
 import type { PlayingCardMeta } from "../types";
 
-export const bangBase: Omit<PlayingCardMeta, "value" | "cardInstanceId"> = {
-  cardTypeId: "bang",
-  image: "bang_1",
-  border: "yellow",
-  description: [
-    [
-      { type: "symbol", key: "bang" },
-      { type: "symbol", key: "any_in_range" },
+export const bangBase: Omit<PlayingCardMeta, "rankAndSuit" | "cardInstanceId"> =
+  {
+    cardTypeId: "bang",
+    image: "bang_1",
+    border: "yellow",
+    description: [
+      [
+        { type: "symbol", key: "bang" },
+        { type: "symbol", key: "any_in_range" },
+      ],
     ],
-  ],
-  tooltipIcon: false,
-  pack: "base",
-};
+    tooltipIcon: false,
+    pack: "base",
+  };
 
-export const barrelBase: Omit<PlayingCardMeta, "value" | "cardInstanceId"> = {
+export const barrelBase: Omit<
+  PlayingCardMeta,
+  "rankAndSuit" | "cardInstanceId"
+> = {
   cardTypeId: "barrel",
   image: "barrel",
   border: "blue",
@@ -29,40 +33,47 @@ export const barrelBase: Omit<PlayingCardMeta, "value" | "cardInstanceId"> = {
   pack: "base",
 };
 
-export const beerBase: Omit<PlayingCardMeta, "value" | "cardInstanceId"> = {
-  cardTypeId: "beer",
-  image: "beer",
-  border: "yellow",
-  description: [[{ type: "symbol", key: "beer" }]],
-  tooltipIcon: false,
-  pack: "base",
-};
-
-export const cat_balouBase: Omit<PlayingCardMeta, "value" | "cardInstanceId"> =
+export const beerBase: Omit<PlayingCardMeta, "rankAndSuit" | "cardInstanceId"> =
   {
-    cardTypeId: "cat_balou",
-    image: "cat_balou",
+    cardTypeId: "beer",
+    image: "beer",
     border: "yellow",
-    description: [
-      [
-        { type: "symbol", key: "discard" },
-        { type: "symbol", key: "any" },
-      ],
-    ],
+    description: [[{ type: "symbol", key: "heal" }]],
     tooltipIcon: false,
     pack: "base",
   };
 
-export const duelBase: Omit<PlayingCardMeta, "value" | "cardInstanceId"> = {
-  cardTypeId: "duel",
-  image: "duel",
+export const cat_balouBase: Omit<
+  PlayingCardMeta,
+  "rankAndSuit" | "cardInstanceId"
+> = {
+  cardTypeId: "cat_balou",
+  image: "cat_balou",
   border: "yellow",
-  description: [[{ type: "text", key: "duel" }]],
-  tooltipIcon: true,
+  description: [
+    [
+      { type: "symbol", key: "discard" },
+      { type: "symbol", key: "any" },
+    ],
+  ],
+  tooltipIcon: false,
   pack: "base",
 };
 
-export const dynamiteBase: Omit<PlayingCardMeta, "value" | "cardInstanceId"> = {
+export const duelBase: Omit<PlayingCardMeta, "rankAndSuit" | "cardInstanceId"> =
+  {
+    cardTypeId: "duel",
+    image: "duel",
+    border: "yellow",
+    description: [[{ type: "text", key: "duel" }]],
+    tooltipIcon: true,
+    pack: "base",
+  };
+
+export const dynamiteBase: Omit<
+  PlayingCardMeta,
+  "rankAndSuit" | "cardInstanceId"
+> = {
   cardTypeId: "dynamite",
   image: "dynamite",
   border: "blue",
@@ -77,7 +88,10 @@ export const dynamiteBase: Omit<PlayingCardMeta, "value" | "cardInstanceId"> = {
   pack: "base",
 };
 
-export const gatlingBase: Omit<PlayingCardMeta, "value" | "cardInstanceId"> = {
+export const gatlingBase: Omit<
+  PlayingCardMeta,
+  "rankAndSuit" | "cardInstanceId"
+> = {
   cardTypeId: "gatling",
   image: "gatling",
   border: "yellow",
@@ -93,7 +107,7 @@ export const gatlingBase: Omit<PlayingCardMeta, "value" | "cardInstanceId"> = {
 
 export const general_storeBase: Omit<
   PlayingCardMeta,
-  "value" | "cardInstanceId"
+  "rankAndSuit" | "cardInstanceId"
 > = {
   cardTypeId: "general_store",
   image: "general_store",
@@ -103,7 +117,10 @@ export const general_storeBase: Omit<
   pack: "base",
 };
 
-export const indiansBase: Omit<PlayingCardMeta, "value" | "cardInstanceId"> = {
+export const indiansBase: Omit<
+  PlayingCardMeta,
+  "rankAndSuit" | "cardInstanceId"
+> = {
   cardTypeId: "indians",
   image: "indians",
   border: "yellow",
@@ -112,22 +129,26 @@ export const indiansBase: Omit<PlayingCardMeta, "value" | "cardInstanceId"> = {
   pack: "base",
 };
 
-export const jailBase: Omit<PlayingCardMeta, "value" | "cardInstanceId"> = {
-  cardTypeId: "jail",
-  image: "jail",
-  border: "blue",
-  description: [
-    [
-      { type: "symbol", key: "range_hearts" },
-      { type: "symbol", key: "equals" },
-      { type: "text", key: "jail" },
+export const jailBase: Omit<PlayingCardMeta, "rankAndSuit" | "cardInstanceId"> =
+  {
+    cardTypeId: "jail",
+    image: "jail",
+    border: "blue",
+    description: [
+      [
+        { type: "symbol", key: "range_hearts" },
+        { type: "symbol", key: "equals" },
+        { type: "text", key: "jail" },
+      ],
     ],
-  ],
-  tooltipIcon: true,
-  pack: "base",
-};
+    tooltipIcon: true,
+    pack: "base",
+  };
 
-export const missedBase: Omit<PlayingCardMeta, "value" | "cardInstanceId"> = {
+export const missedBase: Omit<
+  PlayingCardMeta,
+  "rankAndSuit" | "cardInstanceId"
+> = {
   cardTypeId: "missed",
   image: "missed",
   border: "yellow",
@@ -136,7 +157,10 @@ export const missedBase: Omit<PlayingCardMeta, "value" | "cardInstanceId"> = {
   pack: "base",
 };
 
-export const mustangBase: Omit<PlayingCardMeta, "value" | "cardInstanceId"> = {
+export const mustangBase: Omit<
+  PlayingCardMeta,
+  "rankAndSuit" | "cardInstanceId"
+> = {
   cardTypeId: "mustang",
   image: "mustang",
   border: "blue",
@@ -145,7 +169,10 @@ export const mustangBase: Omit<PlayingCardMeta, "value" | "cardInstanceId"> = {
   pack: "base",
 };
 
-export const panicBase: Omit<PlayingCardMeta, "value" | "cardInstanceId"> = {
+export const panicBase: Omit<
+  PlayingCardMeta,
+  "rankAndSuit" | "cardInstanceId"
+> = {
   cardTypeId: "panic",
   image: "panic",
   border: "yellow",
@@ -159,23 +186,28 @@ export const panicBase: Omit<PlayingCardMeta, "value" | "cardInstanceId"> = {
   pack: "base",
 };
 
-export const saloonBase: Omit<PlayingCardMeta, "value" | "cardInstanceId"> = {
+export const saloonBase: Omit<
+  PlayingCardMeta,
+  "rankAndSuit" | "cardInstanceId"
+> = {
   cardTypeId: "saloon",
   image: "saloon",
   border: "yellow",
   description: [
     [
-      { type: "symbol", key: "beer" },
+      { type: "symbol", key: "heal" },
       { type: "symbol", key: "all" },
     ],
-    [{ type: "divider", key: "divider" }],
-    [{ type: "symbol", key: "beer" }],
+    [{ type: "symbol", key: "heal" }],
   ],
   tooltipIcon: false,
   pack: "base",
 };
 
-export const scopeBase: Omit<PlayingCardMeta, "value" | "cardInstanceId"> = {
+export const scopeBase: Omit<
+  PlayingCardMeta,
+  "rankAndSuit" | "cardInstanceId"
+> = {
   cardTypeId: "scope",
   image: "scope",
   border: "blue",
@@ -184,24 +216,26 @@ export const scopeBase: Omit<PlayingCardMeta, "value" | "cardInstanceId"> = {
   pack: "base",
 };
 
-export const stagecoachBase: Omit<PlayingCardMeta, "value" | "cardInstanceId"> =
-  {
-    cardTypeId: "stagecoach",
-    image: "stagecoach",
-    border: "yellow",
-    description: [
-      [
-        { type: "symbol", key: "pick" },
-        { type: "symbol", key: "pick" },
-      ],
+export const stagecoachBase: Omit<
+  PlayingCardMeta,
+  "rankAndSuit" | "cardInstanceId"
+> = {
+  cardTypeId: "stagecoach",
+  image: "stagecoach",
+  border: "yellow",
+  description: [
+    [
+      { type: "symbol", key: "draw" },
+      { type: "symbol", key: "draw" },
     ],
-    tooltipIcon: false,
-    pack: "base",
-  };
+  ],
+  tooltipIcon: false,
+  pack: "base",
+};
 
 export const wells_cargoBase: Omit<
   PlayingCardMeta,
-  "value" | "cardInstanceId"
+  "rankAndSuit" | "cardInstanceId"
 > = {
   cardTypeId: "wells_fargo",
   image: "wells_fargo",
@@ -217,7 +251,10 @@ export const wells_cargoBase: Omit<
   pack: "base",
 };
 
-export const carabineBase: Omit<PlayingCardMeta, "value" | "cardInstanceId"> = {
+export const carabineBase: Omit<
+  PlayingCardMeta,
+  "rankAndSuit" | "cardInstanceId"
+> = {
   cardTypeId: "carabine",
   image: "carabine",
   border: "blue",
@@ -226,41 +263,50 @@ export const carabineBase: Omit<PlayingCardMeta, "value" | "cardInstanceId"> = {
   pack: "base",
 };
 
-export const remingtonBase: Omit<PlayingCardMeta, "value" | "cardInstanceId"> =
-  {
-    cardTypeId: "remington",
-    image: "remington",
-    border: "blue",
-    description: [[{ type: "symbol", key: "three" }]],
-    tooltipIcon: false,
-    pack: "base",
-  };
+export const remingtonBase: Omit<
+  PlayingCardMeta,
+  "rankAndSuit" | "cardInstanceId"
+> = {
+  cardTypeId: "remington",
+  image: "remington",
+  border: "blue",
+  description: [[{ type: "symbol", key: "three" }]],
+  tooltipIcon: false,
+  pack: "base",
+};
 
-export const winchesterBase: Omit<PlayingCardMeta, "value" | "cardInstanceId"> =
-  {
-    cardTypeId: "winchester",
-    image: "winchester",
-    border: "blue",
-    description: [[{ type: "symbol", key: "five" }]],
-    tooltipIcon: false,
-    pack: "base",
-  };
+export const winchesterBase: Omit<
+  PlayingCardMeta,
+  "rankAndSuit" | "cardInstanceId"
+> = {
+  cardTypeId: "winchester",
+  image: "winchester",
+  border: "blue",
+  description: [[{ type: "symbol", key: "five" }]],
+  tooltipIcon: false,
+  pack: "base",
+};
 
-export const schofieldBase: Omit<PlayingCardMeta, "value" | "cardInstanceId"> =
-  {
-    cardTypeId: "schofield",
-    image: "schofield",
-    border: "blue",
-    description: [[{ type: "symbol", key: "two" }]],
-    tooltipIcon: false,
-    pack: "base",
-  };
+export const schofieldBase: Omit<
+  PlayingCardMeta,
+  "rankAndSuit" | "cardInstanceId"
+> = {
+  cardTypeId: "schofield",
+  image: "schofield",
+  border: "blue",
+  description: [[{ type: "symbol", key: "two" }]],
+  tooltipIcon: false,
+  pack: "base",
+};
 
-export const volcanicBase: Omit<PlayingCardMeta, "value" | "cardInstanceId"> = {
+export const volcanicBase: Omit<
+  PlayingCardMeta,
+  "rankAndSuit" | "cardInstanceId"
+> = {
   cardTypeId: "volcanic",
   image: "volcanic",
   border: "blue",
-  description: [[{ type: "symbol", key: "volcanic" }]],
+  description: [[{ type: "text", key: "volcanic" }]],
   tooltipIcon: true,
   pack: "base",
 };
