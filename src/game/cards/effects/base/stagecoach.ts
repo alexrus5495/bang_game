7d486a3043c8e4e18910308aac397a9ccadf45cd
alexrus5495 <alexrus5495@gmail.com>
2025-07-28 19:40:53 +0300

    [DEL] game logic moved on server

--- src/game/cards/effects/base/stagecoach.ts
+++
@@ -1,6 +0,0 @@
-import type { Game } from "../../../engine/core/game";
-import type { Player } from "../../../engine/player/player";
-
-export function STAGECOACH(game: Game, player: Player, cardId: string) {
-  game.SC.cards.drawToHand(player, 2);
-}
--- src/game/cards/effects/base/wells_fargo.ts
+++
@@ -1,6 +0,0 @@
-import type { Game } from "../../../engine/core/game";
-import type { Player } from "../../../engine/player/player";
-
-export function WELLS_FARGO(game: Game, player: Player, cardId: string) {
-  game.SC.cards.drawToHand(player, 3);
-}
