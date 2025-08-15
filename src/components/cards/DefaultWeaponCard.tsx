d0cbb778e7a68eb11f27f8989e6d637806753775
alexrus5495 <alexrus5495@gmail.com>
2025-08-15 17:52:58 +0300

    [ADD] finish DefaultWeaponCard component

--- src/components/cards/DefaultWeaponCard.tsx
+++ src/components/cards/DefaultWeaponCard.tsx
@@ -1,3 +1,59 @@
+import { defaultWeaponMeta } from "../../config/defaultWeaponMeta";
+import type { BorderColor, BorderType, PlayingCardMeta } from "../../types";
+import {
+  Image,
+  Border,
+  BulletHolesMask,
+  CardContent,
+  Description,
+  Title,
+} from "./shared";
+import CardScaler from "./shared/CardScaler";
+import {
+  CARD_CONTAINER_BORDER_RADIUS,
+  CARD_CONTAINER_HEIGHT,
+  CARD_CONTAINER_WIDTH,
+} from "./shared/constants";
+
 export default function DefaultWeaponCard() {
-  return <></>;
+  const cardData = defaultWeaponMeta as Omit<
+    PlayingCardMeta,
+    "rankAndSuit" | "cardInstanceId" | "effect" | "_range"
+  >;
+
+  const { cardTypeId, description, image, pack } = cardData;
+  const cardType = "playing";
+
+  const hasBulletHoles = cardData.decorations.includes("bullet_holes");
+
+  const borderColor =
+    "borderColor" in cardData ? cardData.borderColor : undefined;
+  const borderType = "borderType" in cardData ? cardData.borderType : undefined;
+
+  return (
+    <CardScaler>
+      <div
+        className="relative border-2 shadow"
+        style={{
+          height: `${CARD_CONTAINER_HEIGHT}px`,
+          width: `${CARD_CONTAINER_WIDTH}px`,
+          borderRadius: `${CARD_CONTAINER_BORDER_RADIUS}px`,
+        }}
+      >
+        {hasBulletHoles && <BulletHolesMask />}
+
+        {borderColor && (
+          <Border
+            borderColor={borderColor as BorderColor}
+            borderType={borderType as BorderType}
+          />
+        )}
+        <CardContent>
+          <Title pack={pack} cardTypeId={cardTypeId} cardType={cardType} />
+          <Image cardImage={image} />
+          <Description description={description} packKey={pack} />
+        </CardContent>
+      </div>
+    </CardScaler>
+  );
 }
