3ad402cf9e70155b972ff37c341712d9108b12b8
alexrus5495 <alexrus5495@gmail.com>
2026-05-15 03:07:30 +0300

    [REFACTOR] detach and rename hook useAnimationLayer into more fitting useGameEventProcessor

--- src/components/table/AnimationLayer.tsx
+++ src/components/table/AnimationLayer.tsx
@@ -1,14 +1,11 @@
-import { useCallback, useEffect, useState } from "react";
-import { useMessagesState } from "../../hooks/useMessagesState";
-import ANIMATIONS, { type AnimationComponent } from "../../animations";
-import type { SystemMessage } from "../../types";
+import { useGameEventProcessor } from "../../hooks/useGameEventProcessor";
 
 export default function AnimationLayer({
   isAllReady,
 }: {
   isAllReady: boolean;
 }) {
-  const { currentAnimation } = useAnimationLayer();
+  const { currentAnimation } = useGameEventProcessor();
 
   if (!isAllReady) return null;
 
@@ -20,56 +17,3 @@ export default function AnimationLayer({
     </div>
   );
 }
-
-const useAnimationLayer = () => {
-  //WARNING: DEV ONLY, REMOVE AFTER TESTING
-  const messages = useMessagesState()[0];
-  const animationQueue = messages as SystemMessage[];
-
-  const [currentAnimation, setCurrentAnimation] =
-    useState<AnimationComponent | null>(null);
-  const [lastFinishedAnimationId, setLastFinishedAnimationId] = useState<
-    number | null
-  >(null);
-  const onAnimationComplete = useCallback((animationId: number) => {
-    setCurrentAnimation(null);
-    setLastFinishedAnimationId(animationId);
-  }, []);
-
-  useEffect(() => {
-    if (!animationQueue) return;
-
-    const animationToHandle =
-      lastFinishedAnimationId === null
-        ? animationQueue[0]
-        : animationQueue.find(
-            (animation) => animation.id > lastFinishedAnimationId,
-          );
-
-    if (!animationToHandle) return;
-
-    const Component = ANIMATIONS[animationToHandle.template];
-
-    if (!Component) {
-      console.error(
-        `Couldn't find the animation component for ${animationToHandle.template}`,
-      );
-      return;
-    }
-
-    setCurrentAnimation({
-      Component,
-      props: {
-        onComplete: () => onAnimationComplete(animationToHandle.id),
-        data: animationToHandle.data,
-        animationId: animationToHandle.id,
-      },
-    });
-  }, [lastFinishedAnimationId, animationQueue, onAnimationComplete]);
-
-  return {
-    animationQueue,
-    currentAnimation,
-    lastFinishedAnimationId,
-  };
-};
