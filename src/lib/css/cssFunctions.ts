dca1b98021f4de2155d85da800f79136b78ab331
alexrus5495 <alexrus5495@gmail.com>
2026-07-30 18:47:22 +0300

    [ADD] getSizeAdaptivePx util function

--- src/lib/css/cssFunctions.ts
+++ src/lib/css/cssFunctions.ts
@@ -4,3 +4,10 @@
 export function sizeAdaptive(factor: number) {
   return `min(calc(51vw/${factor}), calc(100vh/${factor}))`;
 }
+
+export function getSizeAdaptivePx(factor: number) {
+  return Math.min(
+    (window.innerWidth * 0.51) / factor,
+    window.innerHeight / factor,
+  );
+}
